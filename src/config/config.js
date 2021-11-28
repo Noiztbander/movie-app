039decfd116056feba81243d11839b9aef88ef10
Erick Noiztbander <78412093+Noiztbander@users.noreply.github.com>
2021-11-28 22:44:14 +0100

    minor fixes

--- src/config/config.js
+++ src/config/config.js
@@ -10,6 +10,15 @@ const appConfig = {
       port: process.env.REACT_APP_PORT,
     },
   },
+  production: {
+    movieDB: {
+      MovieDbApiKey: process.env.REACT_APP_MOVIE_DB_API_KEY,
+      MovieDbAccessToken: process.env.REACT_APP_MOVIED_DB_ACCESSTOKEN,
+    },
+    movieApp: {
+      port: process.env.REACT_APP_PORT,
+    },
+  },
 };
 
 export const config = appConfig[NODE_ENV];
