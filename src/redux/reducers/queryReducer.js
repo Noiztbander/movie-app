12e340ed758177b50a635e54fea0972372264d0b
Erick Noiztbander <aventum2@gmail.com>
2024-02-04 14:04:57 +0100

    fix: remove local storage

--- src/redux/reducers/queryReducer.js
+++ src/redux/reducers/queryReducer.js
@@ -1,34 +1,15 @@
-"use client";
-
-let clientWindow = null;
-
-if (typeof window !== "undefined") {
-  clientWindow = window;
-}
-
-let initialState = JSON.parse(
-  clientWindow.localStorage.getItem("app_mediaquery")
-);
-
-if (initialState === null) {
-  initialState = initialState = {
-    superLargeDesktop: true,
-    desktop: true,
-    tablet: true,
-    mobile: true,
-  };
-}
-
+const initialState = {
+  superLargeDesktop: true,
+  desktop: true,
+  tablet: true,
+  mobile: true,
+};
 export const queryReducer = (state = initialState, action) => {
   switch (action.type) {
     case "update/mediaquery":
       const mediaQueryPastState = state;
       const mediaQueryNewState = action.payload;
       Object.assign(mediaQueryPastState, mediaQueryNewState);
-      clientWindow.localStorage.setItem(
-        "app_mediaquery",
-        JSON.stringify({ ...state })
-      );
       return { ...state };
     default:
       return state;
