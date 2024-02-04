a26a04553d01bb92d4e4934c230e26cac63abae0
Erick Noiztbander <aventum2@gmail.com>
2024-02-04 13:48:44 +0100

    fix: update providers to work outside app folder

--- src/HighOrderComponents/MainLayout.js
+++ src/HighOrderComponents/MainLayout.js
@@ -3,6 +3,9 @@
 import React from "react";
 import MainHeader from "../components/MainHeader/MainHeader";
 import MainAside from "../components/MainAside/MainAside";
+import { reduxStore } from "@/redux/store";
+import { Provider } from "react-redux";
+import MoviesProvider from "@/api/movies_provider/MoviesProvider";
 import "./MainLayout.css";
 
 function getDisplayName(WrappedComponent) {
@@ -16,11 +19,19 @@ function MainLayout(WrappedComponent) {
 
   function WrapperComponent({ ...props }) {
     return (
-      <section className="MainLayout">
-        <MainHeader />
-        <MainAside />
-        <WrappedComponent {...props} />
-      </section>
+      <Provider store={reduxStore || {}}>
+        <MoviesProvider>
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            async
+          />
+          <section className="MainLayout">
+            <MainHeader />
+            <MainAside />
+            <WrappedComponent {...props} />
+          </section>
+        </MoviesProvider>
+      </Provider>
     );
   }
 
